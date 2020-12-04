a81b5969d0b27fa0ced0c45cfa0ecab509ab6b5e
iatanasov77 <i.atanasov77@gmail.com>
2020-12-04 09:35:26 +0100

    Commit

--- templates/vs-list-dlls.sh
+++ templates/vs-list-dlls.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Usage: vs-list-dlls.sh /projects/MyProject/build/release/MyProject.exe
+
 # HOWTO: https://stackoverflow.com/questions/29678352/how-to-use-qts-windeployqt-in-linux-fedora
-WINEDEBUG=+loaddll wine ./myapp 2> dll.log
+WINEDEBUG=+loaddll wine $1 2> dll.log
 grep Loaded dll.log | grep -v 'system32\|:load_builtin_dll' \
   | awk -F'"' '{print $2}' \
   | sed -e 's@\\\\@/@g' -e 's/^[A-Z]://' \
   | sort > dll.lst
+  
+echo "List writed to 'dll.lst'"
