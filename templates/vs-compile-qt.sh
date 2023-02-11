ced3ba013ddcb6946490c32c888eab7099a16695
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-02-11 11:49:32 +0200

    Fix QT Compilation Scripts.

--- templates/vs-compile-qt.sh
+++ templates/vs-compile-qt.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-cd "{{toolsdir}}/qt5-src/"
-
 ##########################################################################
 # Manual How to Build: https://wiki.qt.io/Building_Qt_5_from_Git/bg
 ##########################################################################
 
-# Clean Source Tree
-#git submodule foreach 'git clean -dffx'
+cd "{{toolsdir}}/qt-build/"
+rm -rf ./*
+rm -rf "{{toolsdir}}/qt-package/*"
 
-./init-repository --module-subset=default,-qtwebengine --branch
-./configure -prefix "{{toolsdir}}/qt-{{vs_qt_version_full}}/x86_64/" \
+#./init-repository --module-subset=default,-qtwebengine --branch
+../qt5-src/configure -prefix "{{toolsdir}}/qt-package/" \
 	#-recheck-all \
-	-release -opensource -confirm-license -make libs -make tools \
+	-release -opensource -confirm-license \
+	-make libs -make tools \
 	-opengl desktop -platform linux-g++-64 \
 	-nomake tests -nomake examples \
 	-sql-mysql \
 	-feature-library \
-	-skip qtwebengine -skip qtdocgallery
+	-skip qtwebengine -skip qtdocgallery -skip qtlocation
 	
-#make -j8
 make -j$(nproc)
-#make module-qtspeech
-#make module-qttools
 make install
-
-pushd "{{toolsdir}}/qt-{{vs_qt_version_full}}/x86_64/"
-tar cfj "{{homedir}}/qt-{{vs_qt_version_full}}-x86_64.tbz2" "."
-popd
