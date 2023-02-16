256de47e87fae21834583e1e5770eeae1242fcf7
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-02-16 15:39:31 +0200

    Fix Template For QT6 MinGw Cross-Compilation Command.

--- templates/vs-cross-compile-qt6.sh
+++ templates/vs-cross-compile-qt6.sh
@@ -23,8 +23,8 @@ rm -rf "{{toolsdir}}/qt-build/package/*"
 	-skip qtimageformats -skip qtnetworkauth -skip qtquickcontrols2 \
 	-skip qtsensors -skip qtwayland \
 	-no-feature-thread \
-	-qt-host-path "{{toolsdir}}/qt-build/host-target" \
-	-- -DCMAKE_TOOLCHAIN_FILE=/usr/local/vs_cmake/mingw-w64-x86_64.cmake -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON
+	-qt-host-path "{{toolsdir}}/qt-{{vs_qt_version_full}}/x86_64/" \
+	-- -DCMAKE_TOOLCHAIN_FILE=/usr/local/vs_cmake/mingw-w64-x86_64.cmake # -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON
 
 cmake --build . --verbose
 cmake --install . --verbose
