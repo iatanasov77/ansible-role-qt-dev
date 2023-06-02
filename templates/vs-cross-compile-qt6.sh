d8606a9e02060a3db9266eaed8cd65ad2af9fcbf
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-06-02 07:38:05 +0300

    Improve QT Cross Compile Script for QT6.

--- templates/vs-cross-compile-qt6.sh
+++ templates/vs-cross-compile-qt6.sh
@@ -9,20 +9,23 @@ rm -rf "{{toolsdir}}/qt-build/package/*"
 	-opengl desktop -xplatform win32-g++ \
 	-nomake tests -nomake examples \
 	-skip qtwebengine -skip qtdocgallery -skip qtlocation \
-	-skip qtx11extras \
-	-skip qt3d -skip qtquick3d -skip qtquick3dphysics -skip qtcanvas3d -skip qtgraphicaleffects -skip qtmultimedia \
+	-skip qtwinextras -skip qtx11extras -skip qtmacextras -skip qtandroidextras \
+	-skip qt3d -skip qtquick3d -skip qtquick3dphysics -skip qtcanvas3d -skip qtgraphicaleffects \
+	-skip qtdeclarative \
+	-skip qtdoc -skip qtlottie -skip qtmqtt -skip qtopcua -skip qtscxml -skip qtvirtualkeyboard \
+	-skip qtwebview \
+	-skip qtquicktimeline -skip qtquickeffectmaker \
 	-skip qtwebsockets -skip qtactiveqt \
-	-skip qtcharts -skip qtdoc \
-	-skip qtremoteobjects -skip qtserialbus -skip qtwebchannel \
-	-skip qtwebview -skip qtandroidextras -skip qtconnectivity \
-	-skip qtgamepad -skip qtmacextras -skip qtpurchasing -skip qtscript \
-	-skip qttranslations -skip qtwinextras \
-	-skip qtdatavis3d \
-	-skip qtquickcontrols -skip qtscxml \
-	-skip qtvirtualkeyboard -skip qtwebglplugin \
+	-skip qtcharts -skip qtremoteobjects -skip qtserialbus -skip qtwebchannel \
+	-skip qtwebview -skip qtconnectivity \
+	-skip qtgamepad -skip qtpurchasing -skip qtscript \
+	-skip qttranslations -skip qtdatavis3d \
+	-skip qtquickcontrols -skip qtwebglplugin \
 	-skip qtimageformats -skip qtnetworkauth -skip qtquickcontrols2 \
 	-skip qtsensors -skip qtwayland \
+	-skip qtsvg -skip qtshadertools -skip qtmultimedia \
 	-no-feature-thread \
+	-no-feature-networklistmanager \
 	-qt-host-path "{{toolsdir}}/qt-{{vs_qt_version_full}}/x86_64/" \
 	-- -DCMAKE_TOOLCHAIN_FILE=/usr/local/vs_cmake/mingw-w64-x86_64.cmake # -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON
 
